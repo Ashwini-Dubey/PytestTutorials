72b3ef65e0c6fe7d4c9b03022cc4c9e1ee05926b
Ashwini Dubey <ak213414@gmail.com>
2025-02-09 01:47:04 +0530

    Pytest Beginner

---
+++ test_with_fixtures_multiple_tests.py
@@ -0,0 +1,11 @@
+import pytest
+
+@pytest.fixture()
+def sample_data():
+    return {"name":"Ashwini","age":28}
+
+def test_fixture(sample_data):
+    assert sample_data["name"] == "Ashwini"
+
+def test_fixture_multiple(sample_data):
+    assert sample_data["age"] == 28
